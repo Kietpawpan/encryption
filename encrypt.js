bb5b829fe1a80e3e05f8776389784a4cb0721f41
Monte Kietpawpan <kietpawpan@gmail.com>
2024-08-04 18:11:26 +0700

    Update encrypt.js

--- encrypt.js
+++ encrypt.js
@@ -9,6 +9,55 @@ $(document).on('input', 'textarea', function () {
     }); 
 }
 
+function getUTF8Length() {
+  var s = document.getElementById('key1').value;
+  var len = 0;
+  for (var i = 0; i < s.length; i++) {
+    var code = s.charCodeAt(i);
+    if (code <= 0x7f) {
+      len += 1;
+    } else if (code <= 0x7ff) {
+      len += 2;
+    } else if (code >= 0xd800 && code <= 0xdfff) {
+      // Surrogate pair: These take 4 bytes in UTF-8 and 2 chars in UCS-2
+      // (Assume next char is the other [valid] half and just skip it)
+      len += 4; i++;
+    } else if (code < 0xffff) {
+      len += 3;
+    } else {
+      len += 4;
+    }
+  }
+  return len;
+}
+
+/* https://stackoverflow.com/a/64802091 Default key length and aes options*/
+
+function setHx() {
+$(document).on('input', 'textarea', function () {
+        $(this).outerHeight(38).outerHeight(this.scrollHeight); // 38 or '1em' -min-height
+    }); 
+var key = document.getElementById('key1').value;
+var b = getUTF8Length(key)*8;
+var keyLength ="";
+	if(b<16){keyLength='<font color="red"><i class="fa fa-warning "></i> < 16 byte long key!</font>';}
+	else if(b<24){keyLength='<font color="green">16 byte long key</font>';}
+	else if(b<32){keyLength='<font color="green">24 byte long key</font>';}
+	else{keyLength='<font color="green">32 byte long key</font>';}
+
+var aes ="";
+	if(b<16){aes ='<font color="red"><i class="fa fa-unlock "></i> ENCRYPT</font>';}
+	else if(b<24){aes='<i class="fa fa-lock "></i> AES-128';}
+	else if(b<32){aes='<i class="fa fa-lock "></i> AES-192';}
+	else{aes='<i class="fa fa-lock "></i> AES-256';}
+
+
+
+document.getElementById('bit').innerHTML = keyLength;
+document.getElementById('enc').innerHTML = aes;
+
+}
+
 function setPaste() {
    navigator.clipboard.readText()
 	.then(text => {
@@ -117,9 +166,6 @@ function copyGuid(){
 }
 
 function clearAll(){
-	/*
-location.reload();
-*/
 document.getElementById('key1').value ="";
 document.getElementById('key2').value ="";
 document.getElementById('3').innerHTML ="";
@@ -129,6 +175,7 @@ document.getElementById('data2').value ="";
 document.getElementById('data3').value ="";
 document.getElementById('data4').value ="";
 document.getElementById('data1').value ="";
+document.getElementById('bit').innerHTML ="";
 }
 
 
@@ -167,3 +214,9 @@ else {
         element.addEventListener(event, handler, false);
     };
 }
+
+/*http://stackoverflow.com/a/12206089/993683 
+by @broofa)*/
+
+  
+ 
